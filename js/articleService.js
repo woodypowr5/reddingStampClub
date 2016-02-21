d04d6ef16159edb9ee9c25e53a031f14b545fccc
woodypowr5 <cwoodward5@hotmail.com>
2016-02-20 20:17:39 -0800

    added gradient less file and applied to refere

--- js/articleService.js
+++ js/articleService.js
@@ -24,69 +24,33 @@ app.service("articleService", ['$rootScope', function($rootScope){
 
     this.articles.push(
         {
-            "title":    "I just love cheese",
-            "date":     "Jan 16, 2016",
-            "bodyText": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris. ",
-            "referer":  "",
-            "src":      "",
-            "image":    {
-                "src":          "",
-                "alt":          "",
-                "position":     ""
-            },
-            "options":  {
+          "title":    "UN Special stamps to celebrate the diversity of the LGBT community",
+          "date":     "Feb 19, 2016",
+          "bodyText": "United Nations issued special stamps to celebrate the diversity of the LGBT community and to promotes the UN Free & Equal campaign for LGBT equality.",
+          "referer":  "Philatelynews.com",
+          "src":      "http://philatelynews.com/un-special-stamps-to-celebrate-the-diversity-of-the-lgbt-community/",
+          "image":    {
+              "src":          "assets/img/articles/LGBT-diversity-cropped.jpg",
+              "alt":          "Queen Canadian Stamp",
+              "position":     "top-inset"
+          },
+          "options":  {
 
-            }
-        }
+          }
+      }
     );
 
     this.articles.push(
         {
-            "title":    "I love your mom too",
-            "date":     "Jan 12, 2016",
-            "bodyText": "hitecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exer",
-            "referer":  "",
-            "src":      "",
+            "title":    "Australian Decimal currency 50 years celebrated through Special stamp",
+            "date":     "Feb 15, 2016",
+            "bodyText": "Australia post marks 50 years of Australian Decimal currency that supplanted pounds, shillings and pence in Australia on 14 February 1966.The $1 stamp features the images of new $1 banknote and coin along with the image of £1 banknote which was replaced.On 14 February 1966, one era ended and another began when Australia replaced British-style currency with decimal notes and coins.",
+            "referer":  "Philatelynews.com",
+            "src":      "http://philatelynews.com/australian-decimal-currency/",
             "image":    {
-                "src":          "",
+                "src":          "assets/img/articles/australia-cropped.jpg",
                 "alt":          "",
-                "position":     ""
-            },
-            "options":  {
-
-            }
-        }
-    );
-
-    this.articles.push(
-        {
-            "title":    "One time I farted",
-            "date":     "Feb 02, 2016",
-            "bodyText": "Same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is",
-            "referer":  "",
-            "src":      "",
-            "image":    {
-                "src":          "",
-                "alt":          "",
-                "position":     ""
-            },
-            "options":  {
-
-            }
-        }
-    );
-
-    this.articles.push(
-        {
-            "title":    "I love your mom too",
-            "date":     "Jan 12, 2016",
-            "bodyText": "hitecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exer",
-            "referer":  "",
-            "src":      "",
-            "image":    {
-                "src":          "",
-                "alt":          "",
-                "position":     ""
+                "position":     "top-inset"
             },
             "options":  {
 
@@ -99,12 +63,48 @@ app.service("articleService", ['$rootScope', function($rootScope){
             "title":    "One time I farted",
             "date":     "Feb 02, 2016",
             "bodyText": "Same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is",
-            "referer":  "",
+            "referer":  "Philatelynews.com",
             "src":      "",
             "image":    {
                 "src":          "",
                 "alt":          "",
-                "position":     ""
+                "position":     "none"
+            },
+            "options":  {
+
+            }
+        }
+    );
+
+    this.articles.push(
+        {
+            "title":    "USPS promises the moon with global forever stamp",
+            "date":     "Jan 12, 2016",
+            "bodyText": "The newest round global forever stamp from the United States Postal Service pictures Earth’s moon. Features of the lunar surface are easily seen in the detailed image created from an existing photograph by Beth Swanson. The stamp and pane selvage were designed by Greg Breeding. William J. Gicker was the USPS art director for this issue.",
+            "referer":  "Linns.com",
+            "src":      "https://www.linns.com/news/us-stamps-postal-history/2016/february/usps-promises-the-moon-with-global-forever-stamp.html",
+            "image":    {
+                "src":          "assets/img/articles/global-forever-cropped.jpg",
+                "alt":          "",
+                "position":     "left"
+            },
+            "options":  {
+                "width":    "double"
+            }
+        }
+    );
+
+    this.articles.push(
+        {
+            "title":    "One time I farted",
+            "date":     "Feb 02, 2016",
+            "bodyText": "Same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is",
+            "referer":  "Philatelynews.com",
+            "src":      "",
+            "image":    {
+                "src":          "",
+                "alt":          "",
+                "position":     "none"
             },
             "options":  {
 
@@ -117,12 +117,12 @@ app.service("articleService", ['$rootScope', function($rootScope){
             "title":    "I love your mom too",
             "date":     "Jan 12, 2016",
             "bodyText": "hitecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exer",
-            "referer":  "",
+            "referer":  "Philatelynews.com",
             "src":      "",
             "image":    {
                 "src":          "",
                 "alt":          "",
-                "position":     ""
+                "position":     "none"
             },
             "options":  {
 
