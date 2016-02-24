6427eff946037d5deef8dd9a2d9a817b189021b0
woodypowr5 <cwoodward5@hotmail.com>
2016-02-23 16:28:12 -0800

    added link list to linkService

--- js/linkService.js
+++ js/linkService.js
@@ -3,19 +3,156 @@ app.service("linkService", ['$rootScope', function($rootScope){
       
     this.links = [];
 
-    for (var i = 0; i < 10; i++) {
+    // for (var i = 0; i < 10; i++) {
         
-        this.links.push(
-            {
-                "title":            "im the title",
-                "description":      "This is a short, one line summary of the link",
-                "image": {
-                    "src":      "blah",
-                    "alt":      "blah"
+    //     this.links.push(
+    //         {
+    //             "title":            "im the title",
+    //             "description":      "This is a short, one line summary of the link",
+    //             "image": {
+    //                 "src":      "blah",
+    //                 "alt":      "blah"
+    //             }
+    //         }
+    //     );
+    // };
+
+    /*
+            
+        
+
+    [
+        {
+            "category":     "National and Regional Societies",
+            "links":        [
+                {
+                    "title":        "American Philiatelic Society",
+                    "description":  "",
+                    "about":        "",
+                    "imageSrc":     "",
+                    "url":          "http://stamps.org/Home/"
+                },
+                {
+                    "title":        "American Air Mail Society",
+                    "description":  "",
+                    "about":        "",
+                    "imageSrc":     "",
+                    "url":          "http://www.americanairmailsociety.org/"
+                },
+                {
+                    "title":        "Council of Northern California Philatelic Societies",
+                    "description":  "",
+                    "about":        "",
+                    "imageSrc":     "",
+                    "url":          "http://norcalstamps.org/"
                 }
-            }
-        );
-    };
+            ]
+        },
+        {    
+            "category":     "Dealers and Sales",
+            "links":        [
+                {
+                    "title":        "National Stamp Dealer's Association",
+                    "description":  "",
+                    "about":        "",
+                    "imageSrc":     "",
+                    "url":          "http://www.nsdainc.org/"
+                }
+            ]
+        },
+        {    
+            "category":     "News & Informational",
+            "links":        [
+                {
+                    "title":        "Linn's Stamp News",
+                    "description":  "",
+                    "about":        "",
+                    "imageSrc":     "",
+                    "url":          "https://www.linns.com/"
+                },
+                {
+                    "title":        "PhilatelyNews",
+                    "description":  "",
+                    "about":        "",
+                    "imageSrc":     "",
+                    "url":          "http://philatelynews.com"
+                },
+                {
+                    "title":        "PositivelyPostal",
+                    "description":  "",
+                    "about":        "",
+                    "imageSrc":     "",
+                    "url":          "http://positivelypostal.com/"
+                }
+            ]
+        },
+        {    
+            "category":     "Authentication and Expertizing",
+            "links":        [
+                {
+                    "title":        "The Philatelic Foundation",
+                    "description":  "",
+                    "about":        "",
+                    "imageSrc":     "",
+                    "url":          "http://www.philatelicfoundation.org/"
+                }
+            ]
+        },
+        {
+            "category":     "Research and Education",
+            "links":        [
+                {
+                    "title":        "Western Philatelic Library",
+                    "description":  "",
+                    "about":        "",
+                    "imageSrc":     "",
+                    "url":          "http://www.fwpl.org/"
+                },
+                {
+                    "title":        "ISWSC Stamp Identifier",
+                    "description":  "",
+                    "about":        "",
+                    "imageSrc":     "",
+                    "url":          "http://www.iswsc.org/iswsc_identifier.html"
+                },
+                {
+                    "title":        "Stamp World Catalogue",
+                    "description":  "",
+                    "about":        "",
+                    "imageSrc":     "",
+                    "url":          "http://www.stampworld.com/en_US/maps/Europe/"
+                }
+            ]
+        },
+        {
+            "category":     "Exhibitions",
+            "links":        [
+                {
+                    "title":        "WESTPEX",
+                    "description":  "",
+                    "about":        "",
+                    "imageSrc":     "",
+                    "url":          "http://www.westpex.com/"
+                },
+                {
+                    "title":        "Filatelic Fiesta",
+                    "description":  "",
+                    "about":        "",
+                    "imageSrc":     "",
+                    "url":          "https://filatelicfiesta.wordpress.com/"
+                },
+                {
+                    "title":        "NOVAPEX",
+                    "description":  "",
+                    "about":        "",
+                    "imageSrc":     "",
+                    "url":          "./novapex"
+                }
+            ]
+        }
+    ]    
+   
+    */
    
 
     var that = this;
